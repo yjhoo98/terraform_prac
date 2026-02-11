76deb2721c53a7a2c0e0be70966da2c51e486b7d
yjhoo98 <ubuntu@ubuntu-2204.linuxvmimages.local>
2026-02-10 20:25:15 -0500

    day3 fin

--- pr_ch3_1/variables.tf
+++ pr_ch3_1/variables.tf
@@ -31,3 +31,42 @@ variable "ami_id" {
  description = "Ubuntu 22.04 AMI ID region:seoul"
  default = "ami-0bae2335fbe5a4018"
 }
+
+# 환경별 설정 맵
+variable "env_configs" {
+  type = map(object({
+    instance_type = string
+    min_size      = number
+    max_size      = number
+    node_count    = number
+  }))
+
+  default = {
+    dev = {
+      instance_type = "t2.micro"
+      min_size      = 1
+      max_size      = 2
+      node_count    = 1
+    }
+
+    staging = {
+      instance_type = "t3.small"
+      min_size      = 2
+      max_size      = 4
+      node_count    = 2
+    }
+
+    prod = {
+      instance_type = "t3.large"
+      min_size      = 3
+      max_size      = 6
+      node_count    = 3
+    }
+  }
+
+  validation {
+    condition     = contains(keys(var.env_configs), var.environment)
+    error_message = "환경은 dev, staging, prod 중 하나여야 합니다."
+  }
+}
+
